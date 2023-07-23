56ad1444e0203ec09714ee7b8819d291b1d61c0a
Alex-ABWorld <alexbu2015@hotmail.com>
2023-07-23 23:37:03 +0100

    Add locator for panel settings view

---
+++ ABCo.Multicam.UI.Avalonia/Views/Strips/Switcher/SwitcherStripSettingsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ABCo.Multicam.UI.Avalonia.Views.Strips.Switcher
+{
+    public partial class SwitcherStripSettingsView : UserControl
+    {
+        public SwitcherStripSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
