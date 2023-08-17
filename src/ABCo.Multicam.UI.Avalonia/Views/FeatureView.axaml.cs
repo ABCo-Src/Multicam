939b76c324d207ef14e180106426da814b4f7535
Alex-ABWorld <alexbu2015@hotmail.com>
2023-08-17 17:11:09 +0100

    Improve settings panel flow

--- src/ABCo.Multicam.UI.Avalonia/Views/FeatureView.axaml.cs
+++ src/ABCo.Multicam.UI.Avalonia/Views/FeatureView.axaml.cs
@@ -1,4 +1,8 @@
+using ABCo.Multicam.UI.ViewModels;
+using ABCo.Multicam.UI.ViewModels.Features;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace ABCo.Multicam.UI.Avalonia.Views
 {
