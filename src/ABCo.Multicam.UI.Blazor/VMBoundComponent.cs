77f6a577c50522211fb41560495da3457944347f
Alex-ABWorld <alexbu2015@hotmail.com>
2023-09-13 17:23:45 +0100

    Fix Blazor memory leak

--- src/ABCo.Multicam.UI.Blazor/VMBoundComponent.cs
+++ src/ABCo.Multicam.UI.Blazor/VMBoundComponent.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 
 namespace ABCo.Multicam.UI.Blazor
 {
-	public abstract class VMBoundComponent<T> : ComponentBase, IHandleEvent where T : INotifyPropertyChanged
+	public abstract class VMBoundComponent<T> : ComponentBase, IDisposable, IHandleEvent where T : INotifyPropertyChanged
 	{
 		T _vm = default!;
 
@@ -25,11 +25,17 @@ namespace ABCo.Multicam.UI.Blazor
 			}
 		}
 
+		public void Dispose()
+		{
+			if (_vm != null)
+				_vm.PropertyChanged -= CallState;
+		}
+
 		void CallState(object? sender, EventArgs e) => StateHasChanged();
 		async Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem callback, object? arg) => await callback.InvokeAsync(arg);
 	}
 
-	public abstract class AnimBasedVMBoundComponent<T> : ComponentBase, IHandleEvent where T : IAnimationHandlingVM
+	public abstract class AnimBasedVMBoundComponent<T> : ComponentBase, IDisposable, IHandleEvent where T : IAnimationHandlingVM
 	{
 		T _vm = default!;
 
@@ -50,6 +56,12 @@ namespace ABCo.Multicam.UI.Blazor
 				_vm.SetAnimationWaiter(PerformAnimationAndDelay);
 				StateHasChanged();
 			}
+		}
+
+		public void Dispose()
+		{
+			if (_vm != null)
+				_vm.PropertyChanged -= CallState;
 		}
 
 		public abstract Task PerformAnimationAndDelay(string propertyName);
