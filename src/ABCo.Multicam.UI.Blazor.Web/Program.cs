729525a49c161e6f3f2adf8151aa470e9392b4f1
Alex-ABWorld <alexbu2015@hotmail.com>
2023-10-10 17:02:59 +0100

    Fix side panel scrollbar position

--- src/ABCo.Multicam.UI.Blazor.Web/Program.cs
+++ src/ABCo.Multicam.UI.Blazor.Web/Program.cs
@@ -3,14 +3,28 @@ using ABCo.Multicam.Client.Blazor.Services;
 using ABCo.Multicam.Client.Blazor.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ABCo.Multicam.App.Win32.Services;
+using ABCo.Multicam.Client.Blazor;
+using ABCo.Multicam.Client;
+using ABCo.Multicam.Server;
+using ABCo.Multicam.UI.Blazor.Web.Services;
 
+// Setup a server
+var blazorDispatcher = new BlazorMainThreadDispatcher();
+var server = new LocalMulticamServer(
+	s => new WebPlatformInfo(),
+	(c, s) => throw new Exception(),
+	(a, s) => new LocalIPCollection(),
+	blazorDispatcher);
+
+// Setup our web client services
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var clientServiceBuilder = new ClientServicesBuilder(builder.Services, true);
+builder.Services.AddSingleton(p => clientServiceBuilder.Build(p, blazorDispatcher, server, server.ServerInfo.ClientsManager.NewConnectionId()));
+ClientServicesRegister.AddServices(clientServiceBuilder);
+BlazorClientServicesRegister.AddServices(clientServiceBuilder);
 
-builder.RootComponents.Add<App>("#app");
+// And build the app from this
+builder.RootComponents.Add<ABCo.Multicam.Client.Blazor.Index>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton<IPlatformInfo, WebPlatformInfo>();
-builder.Services.AddSingleton<IThreadDispatcher, MainThreadDispatcher>();
-
-//BlazorClientServicesRegister.BuildServiceRegister(builder.Services);
-
 await builder.Build().RunAsync();
---
+++ src/ABCo.Multicam.UI.Blazor.Web/Services/LocalIPCollection.cs
@@ -0,0 +1,11 @@
+﻿using ABCo.Multicam.Server.Hosting.Management;
+using System.Net;
+
+namespace ABCo.Multicam.UI.Blazor.Web.Services
+{
+	public class LocalIPCollection : ILocalIPCollection
+	{
+		public IPAddress[]? GetLoadedAddresses() => Array.Empty<IPAddress>();
+		public void Dispose() { }
+	}
+}
